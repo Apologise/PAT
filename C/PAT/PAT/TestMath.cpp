e5624c35931d1789e9b3db6f8e9c9862d4694394
Apologise <603061018@qq.com>
2017-01-14 16:18:50 +0800

    ff

--- C/PAT/PAT/TestMath.cpp
+++
@@ -1,8 +0,0 @@
-#include<cstdio>
-#include<cmath>
-
-const double pi = acos(-1.0);	//	ÒòÎªcos(pi) = -1
-
-int main(){
-	double db1 = sin(pi*45/180);rr
-}
--- C/PAT/PAT/将P进制变为十进制.cpp
+++
@@ -1,6 +0,0 @@
-int y = 0, product = 1;
-while(x != 0){
-	y = y + (x % 10) * product;
-	x = x / 10;
-	product = product * P;
-}
